953218204e0e7a78b62c6c39a94e83be2b0c4dac
Your Name <you@example.com>
2021-06-22 13:40:52 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:applied-math-coding.github.io.git main
+git push -f git@github.com:applied-math-coding/fractal-trees.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
